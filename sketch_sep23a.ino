86d325772941099837c42b12c051b1a10a5f907a
Nikita Lysikov <55156606+sinusinusa@users.noreply.github.com>
2019-09-23 21:31:02 +0300

    True switch + binking after green and red

--- sketch_sep23a.ino
+++ sketch_sep23a.ino
@@ -9,12 +9,27 @@ void setup() {
 
 void loop() {
   digitalWrite(grn, HIGH);
-  delay(10000);
+  delay(6000);
+  for(int i=8; i != 0; i--){
+    digitalWrite(grn, HIGH);
+    delay(250);
+    digitalWrite(grn, LOW);
+    delay(250);
+  }
   digitalWrite(grn,LOW);
   digitalWrite(yel, HIGH);
   delay(10000);
   digitalWrite(yel,LOW);
   digitalWrite(red,HIGH);
-  delay(10000);
+  delay(6000);
+    for(int i2 = 8; i2 !=0; i2--){
+    digitalWrite(red, HIGH);
+    delay(250);
+    digitalWrite(red, LOW);
+    delay(250);
+  }
   digitalWrite(red,LOW);
+  digitalWrite(yel, HIGH);
+  delay(10000);
+  digitalWrite(yel,LOW);
 }
